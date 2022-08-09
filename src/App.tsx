2d2b29de8d1b6c073d8976629f97b38502f089dc
DiegoBelPe <dbeltranpereira@gmail.com>
2022-08-08 21:44:48 -0500

    add components initials

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App(props) {
+interface Props {
+  title: string;
+}
+
+interface Task {
+  id: number;
+  title: string;
+  description : string;
+  completed: boolean;
+}
+
+export function App({title}: Props) {
+  const [tasks, setTasks] = useState<Task[]>([{
+    id: 1,
+    title : "Learn React title with typescript",
+    description : "Learn React",
+    completed: false
+  }]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      {tasks.map(task => (
+        <div>{task.title}</div>
+      ))}
     </div>
   );
 }
 
-export default App;
+
